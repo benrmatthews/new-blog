08e3dc54a1986c3a39bfc822f092c16b6fbbea3d
Ben Matthews <ben@montfort.io>
2021-04-06 13:15:44 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/_data/blog`,
+        path: `${__dirname}/_data/`,
       },
     },
     {
